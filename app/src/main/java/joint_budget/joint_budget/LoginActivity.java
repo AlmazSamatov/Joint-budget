3586b98cceab06db548711ccabe130ca7fdae6ec
Dilshat Salikhov <dilschat@yandex.ru>
2018-04-17 19:11:02 +0300

    EventsAPI finished. Not tested.

--- app/src/main/java/joint_budget/joint_budget/LoginActivity.java
+++ app/src/main/java/joint_budget/joint_budget/LoginActivity.java
@@ -3,11 +3,7 @@ package joint_budget.joint_budget;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import java.util.ArrayList;
-
 import joint_budget.joint_budget.API.FIrebaseAPI.FirebaseEventsAPI;
-import joint_budget.joint_budget.DataTypes.Event;
-import joint_budget.joint_budget.DataTypes.UserInfo;
 
 /**
  * A login screen that offers login via email/password.
@@ -19,14 +15,6 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         FirebaseEventsAPI api = new FirebaseEventsAPI();
-        Event event = new Event();
-        event.setName("a");
-        UserInfo userInfo = new UserInfo();
-        userInfo.setFirstName("a");
-        ArrayList<UserInfo> list = new ArrayList<>();
-        list.add(userInfo);
-        list.add(userInfo);
-        event.setParticipants(list);
 
 
 //        try {
